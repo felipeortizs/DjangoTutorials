d136c79e6ebc5a863cfeb218cb04f918b6520217
felipeortizs <jfortizs@eafit.edu.co>
2023-09-28 16:34:56 -0500

    add tutorial s10

---
+++ tutorials10/backend/api/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('todos_old/', views.TodoList.as_view(), name='list'),
+    path('todos/', views.ToDoListCreate.as_view(), name='list'),
+    path('todos/<int:pk>', views.ToDoRetrieveUpdateDestroy.as_view()),
+    path('todos/<int:pk>/complete', views.ToDoToggleComplete.as_view()),
+    path('signup/', views.signup, name='signup'),
+    path('login/', views.login, name='login'),
+
+
+]
